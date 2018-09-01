6b422dc6ab9a5d6dbe44cfd84d59333e93c51aa9
unknown <nathan.pabst@gmail.com>
2018-09-01 10:25:05 -0500

    added properties and methods for Arms and Legs

---
+++ LegoFigures/Attributes/Arms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoFigures.Attributes
+{
+    class Arms
+    {
+        public string Color { get; set; }
+
+        public Arms(string color) => Color = color;
+
+        public void KarateChop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LegoFigures/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LegoFigures.Attributes;
+
+namespace LegoFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arms = new Arms("yellow");
+            arms.KarateChop();
+
+            Console.WriteLine("press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
