2e9493799c127ef32a36390cb5e3feb3b0e08bfc
unknown <nathan.pabst@gmail.com>
2018-09-01 18:48:56 -0500

    added additional code to print output for the second head option

--- LegoFigures/Program.cs
+++ LegoFigures/Program.cs
@@ -11,6 +11,9 @@ namespace LegoFigures
             var figure = new MiniFigure(head);
             figure.DoStuff();
 
+            var head2 = new HairyHead();
+            var figure2 = new MiniFigure(head2);
+            figure2.DoStuff();
 
 
             Console.ReadLine();
